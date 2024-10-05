8687899b1d2a7f7edb1038c05ed9b59f1d891286
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-10-05 15:32:57 +0500

    New COmmit

--- src/Components/Hooks/UseState/UseState.jsx
+++ src/Components/Hooks/UseState/UseState.jsx
@@ -12,6 +12,13 @@ const UseState = () => {
   }
   return (
     <>
+      <div className='description_wrapper'>
+        <p className='description'>
+        What is the useState Hook? useState is React Hook that allows you to add state to a functional component. 
+        It returns an array with two values: the current state and a function to update it. 
+        The Hook takes an initial state value as an argument and returns an updated state value whenever the setter function is called.
+        </p>
+      </div>
       <div className="wrapper">
         <button onClick={handleSecond} className='btn btnRed'>-</button>
         <h2 className='counter'>{count}</h2>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,6 +4,9 @@ import UseState from '../Hooks/UseState/UseState'
 const Main = () => {
   return (
     <>
+    <div className='main_title_wrapper'>
+        <h2 className='title'>HOOKS</h2>
+    </div>
       <UseState/>
     </>
   )
