ded4531002b953014f9b1ed57d9345a2cd31273a
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-10-05 15:19:42 +0500

    New Commit

--- src/Components/Hooks/UseState/UseState.jsx
+++ src/Components/Hooks/UseState/UseState.jsx
@@ -5,11 +5,18 @@ const UseState = () => {
     const [count, setCount] = useState(0);
 
     const handleCount = () =>{
-        setCount(count ++)
+        setCount(count + 1)
     }
+    const handleSecond = () =>{
+      setCount(count - 1)
+  }
   return (
     <>
-      <button>{count}</button>
+      <div className="wrapper">
+        <button onClick={handleSecond} className='btn btnRed'>-</button>
+        <h2 className='counter'>{count}</h2>
+        <button onClick={handleCount} className='btn'>+</button>
+      </div>
     </>
   )
 }
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseState from '../Hooks/UseState/UseState'
+
+const Main = () => {
+  return (
+    <>
+      <UseState/>
+    </>
+  )
+}
+
+export default Main
